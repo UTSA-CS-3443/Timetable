3bf6d6708c96bfef8cc8ad598e7499c4374550ea
Connor A <xpq486@my.utsa.edu>
2021-11-26 21:46:39 -0600

    Added fields for labels and circles, organized them, edited main.java to use splitpane.

--- Timetable/src/application/Main.java
+++ Timetable/src/application/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.SplitPane;
 
 
 public class Main extends Application {
@@ -12,16 +12,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try 
 		{
-			AnchorPane root = new AnchorPane();
+			SplitPane root = new SplitPane();
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation( Main.class.getResource("view/CalendarScreen.fxml") );
-			root = (AnchorPane) loader.load();
-			
+			root = (SplitPane) loader.load();
 			Scene scene = new Scene (root);
-			
 			primaryStage.setScene(scene);
-			primaryStage.show()
-;		} catch(Exception e) 
+			primaryStage.show();
+		} 
+		catch(Exception e) 
 		{
 			e.printStackTrace();
 		}
