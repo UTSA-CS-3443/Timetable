c0f54237b3592caf2a9a82ec739d57984b1e7cbc
anthrac1t3 <anthrac1t3@MADIE>
2021-11-28 02:41:25 -0600

    Added todo list veiw and made it functional

--- Timetable/src/application/controller/TodoListController.java
+++ Timetable/src/application/controller/TodoListController.java
@@ -1,22 +1,56 @@
 package application.controller;
 
-import java.util.ArrayList;
-
+import application.Main;
 import application.model.Todo;
+import application.model.TodoList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
-public class TodoListController {
+public class TodoListController{
 	
 	@FXML
-	private TabPane tabPane;
+	private ScrollPane scrollPane;
+	@FXML
+	private GridPane gridPane;
+	@FXML
+	private Button addTodo = new Button();
+	@FXML
+	private TextField addText = new TextField();
 	
-	private ArrayList<Tab> tabs;
+	@FXML
+	public void initialize() {
+		gridPane = new GridPane();		
+		updateTodo(Main.user.getTodo());
+	}
 	
-	public void addTab(Todo todo) {
-		Tab tab = new Tab();
+	@FXML
+	public void addTodoFromButton(MouseEvent e) {
+		Main.user.addTodo(addText.getText());
+		updateTodo(Main.user.getTodo());
+		addText.clear();
+	}
+	
+	public void addTodoFromList(int index, Todo todo) {
+		Label label = new Label(todo.getDesc());
+		CheckBox checkBox = new CheckBox();
+		gridPane.addRow(index, label, checkBox);
+	}
+	
+	public void removeTodo(int index) {
 		
 	}
 	
+	public void updateTodo(TodoList tasks) {
+		gridPane.getChildren().clear();
+		for (int i = 0; i < tasks.getListSize(); i++) {
+			addTodoFromList(i, tasks.getTodo(i));
+		}
+		scrollPane.setContent(gridPane);
+	}
 }
