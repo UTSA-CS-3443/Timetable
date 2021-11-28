7e505c4d5045c48b28f58ffc311d879ca63a8b75
anthrac1t3 <anthrac1t3@MADIE>
2021-11-28 00:18:42 -0600

    Some stuff

--- Timetable/src/application/controller/TodoListController.java
+++ Timetable/src/application/controller/TodoListController.java
@@ -1,5 +1,22 @@
 package application.controller;
 
+import java.util.ArrayList;
+
+import application.model.Todo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
 public class TodoListController {
-	//TODO Make it control stuff
+	
+	@FXML
+	private TabPane tabPane;
+	
+	private ArrayList<Tab> tabs;
+	
+	public void addTab(Todo todo) {
+		Tab tab = new Tab();
+		
+	}
+	
 }
