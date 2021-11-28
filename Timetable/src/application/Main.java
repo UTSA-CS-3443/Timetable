606ec0152fe15828b0776105fb18f8d44a6c6047
Brian Bissland <Brian.Bissland@gmail.com>
2021-11-28 02:49:08 -0600

    I really dislike github, but I am doing this for yall.

--- Timetable/src/application/Main.java
+++ Timetable/src/application/Main.java
@@ -3,11 +3,15 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 
 
 public class Main extends Application {
+	
+	public static Stage stage; //This was added for the sceneSwitcher method.
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try 
@@ -19,6 +23,8 @@ public class Main extends Application {
 			Scene scene = new Scene (root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			stage = primaryStage;
 		} 
 		catch(Exception e) 
 		{
@@ -29,5 +35,35 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	/**
+	 * Brian Bissland Added this
+	 * The sceneSwitcher method takes in the filename we need to switch to.
+	 * Then handles the switching of the scenes.
+	 * 
+	 * @param switchTo, as a String.
+	 *
+	 */
+	public static void sceneSwitcher(String switchTo)
+	{
+		try 
+		{
+			Parent root = FXMLLoader.load(Main.class.getResource("view/" + switchTo));
+			if(switchTo.equals("CalendarScreen.fxml"))
+			{
+				stage.setScene(new Scene(root, 800, 500));
+			}
+			else
+			{
+				stage.setScene(new Scene(root, 800, 800));
+			}
+			
+			stage.show();
+
+		} catch(Exception e) 
+			{
+				e.printStackTrace();
+			}
 	}
 }
---
+++ Timetable/src/application/controller/AddEventController.java
@@ -0,0 +1,94 @@
+package application.controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import application.Main;
+import application.model.Event;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+public class AddEventController implements EventHandler<ActionEvent>
+{
+	//This is the Add Event button
+	@FXML
+	private Button addEvent;
+	@FXML
+	private TextArea descOfEvent;
+	@FXML
+	private ColorPicker eventColor;
+	@FXML
+	private CheckBox eventPriority;
+	@FXML
+	private ComboBox<String> startTime;
+	@FXML
+	private DatePicker eventStartDate;
+	@FXML
+	private DatePicker eventEndDate;
+	@FXML
+	private Label eventDescLabel;
+	@FXML
+	private Label eventTimeLabel;
+	@FXML
+	private Label eventStartDateLabel;
+	@FXML
+	private Label eventEndDateLabel;
+	
+	
+	public void initialize()
+	{
+		//remindHours.setItems(FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"));
+		//remindMinutes.setItems(FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60"));
+		startTime.setItems(FXCollections.observableArrayList("12:00 AM", "12:05 AM", "12:10 AM", "12:15 AM", "12:20 AM", "12:25 AM", "12:30 AM", "12:35 AM", "12:40 AM", "12:45 AM", "12:50 AM", "12:55 AM", "1:00 AM", "1:05 AM", "1:10 AM", "1:15 AM", "1:20 AM", "1:25 AM", "1:30 AM", "1:35 AM", "1:40 AM", "1:45 AM", "1:50 AM", "1:55 AM", "2:00 AM", "2:05 AM", "2:10 AM", "2:15 AM", "2:20 AM", "2:25 AM", "2:30 AM", "2:35 AM", "2:40 AM", "2:45 AM", "2:50 AM", "2:55 AM", "3:00 AM", "3:05 AM", "3:10 AM", "3:15 AM", "3:20 AM", "3:25 AM", "3:30 AM", "3:35 AM", "3:40 AM", "3:45 AM", "3:50 AM", "3:55 AM", "4:00 AM", "4:05 AM", "4:10 AM", "4:15 AM", "4:20 AM", "4:25 AM", "4:30 AM", "4:35 AM", "4:40 AM", "4:45 AM", "4:50 AM", "4:55 AM", "5:00 AM", "5:05 AM", "5:10 AM", "5:15 AM", "5:20 AM", "5:25 AM", "5:30 AM", "5:35 AM", "5:40 AM", "5:45 AM", "5:50 AM", "5:55 AM", "6:00 AM", "6:05 AM", "6:10 AM", "6:15 AM", "6:20 AM", "6:25 AM", "6:30 AM", "6:35 AM", "6:40 AM", "6:45 AM", "6:50 AM", "6:55 AM", "7:00 AM", "7:05 AM", "7:10 AM", "7:15 AM", "7:20 AM", "7:25 AM", "7:30 AM", "7:35 AM", "7:40 AM", "7:45 AM", "7:50 AM", "7:55 AM", "8:00 AM", "8:05 AM", "8:10 AM", "8:15 AM", "8:20 AM", "8:25 AM", "8:30 AM", "8:35 AM", "8:40 AM", "8:45 AM", "8:50 AM", "8:55 AM", "9:00 AM", "9:05 AM", "9:10 AM", "9:15 AM", "9:20 AM", "9:25 AM", "9:30 AM", "9:35 AM", "9:40 AM", "9:45 AM", "9:50 AM", "9:55 AM", "10:00 AM", "10:05 AM", "10:10 AM", "10:15 AM", "10:20 AM", "10:25 AM", "10:30 AM", "10:35 AM", "10:40 AM", "10:45 AM", "10:50 AM", "10:55 AM", "11:00 AM", "11:05 AM", "11:10 AM", "11:15 AM", "11:20 AM", "11:25 AM", "11:30 AM", "11:35 AM", "11:40 AM", "11:45 AM", "11:50 AM", "11:55 AM", "12:00 PM", "12:05 PM", "12:10 PM", "12:15 PM", "12:20 PM", "12:25 PM", "12:30 PM", "12:35 PM", "12:40 PM", "12:45 PM", "12:50 PM", "12:55 PM", "1:00 PM", "1:05 PM", "1:10 PM", "1:15 PM", "1:20 PM", "1:25 PM", "1:30 PM", "1:35 PM", "1:40 PM", "1:45 PM", "1:50 PM", "1:55 PM", "2:00 PM", "2:05 PM", "2:10 PM", "2:15 PM", "2:20 PM", "2:25 PM", "2:30 PM", "2:35 PM", "2:40 PM", "2:45 PM", "2:50 PM", "2:55 PM", "3:00 PM", "3:05 PM", "3:10 PM", "3:15 PM", "3:20 PM", "3:25 PM", "3:30 PM", "3:35 PM", "3:40 PM", "3:45 PM", "3:50 PM", "3:55 PM", "4:00 PM", "4:05 PM", "4:10 PM", "4:15 PM", "4:20 PM", "4:25 PM", "4:30 PM", "4:35 PM", "4:40 PM", "4:45 PM", "4:50 PM", "4:55 PM", "5:00 PM", "5:05 PM", "5:10 PM", "5:15 PM", "5:20 PM", "5:25 PM", "5:30 PM", "5:35 PM", "5:40 PM", "5:45 PM", "5:50 PM", "5:55 PM", "6:00 PM", "6:05 PM", "6:10 PM", "6:15 PM", "6:20 PM", "6:25 PM", "6:30 PM", "6:35 PM", "6:40 PM", "6:45 PM", "6:50 PM", "6:55 PM", "7:00 PM", "7:05 PM", "7:10 PM", "7:15 PM", "7:20 PM", "7:25 PM", "7:30 PM", "7:35 PM", "7:40 PM", "7:45 PM", "7:50 PM", "7:55 PM", "8:00 PM", "8:05 PM", "8:10 PM", "8:15 PM", "8:20 PM", "8:25 PM", "8:30 PM", "8:35 PM", "8:40 PM", "8:45 PM", "8:50 PM", "8:55 PM", "9:00 PM", "9:05 PM", "9:10 PM", "9:15 PM", "9:20 PM", "9:25 PM", "9:30 PM", "9:35 PM", "9:40 PM", "9:45 PM", "9:50 PM", "9:55 PM", "10:00 PM", "10:05 PM", "10:10 PM", "10:15 PM", "10:20 PM", "10:25 PM", "10:30 PM", "10:35 PM", "10:40 PM", "10:45 PM", "10:50 PM", "10:55 PM", "11:00 PM", "11:05 PM", "11:10 PM", "11:15 PM", "11:20 PM", "11:25 PM", "11:30 PM", "11:35 PM", "11:40 PM", "11:45 PM", "11:50 PM", "11:55 PM"));
+	}
+	
+	public void handle(ActionEvent arg0) // Unused
+	{
+	}
+	
+	public void addEventClicked(MouseEvent event)
+	{
+		if(eventStartDate.getValue().equals("")){eventStartDateLabel.setTextFill(Color.RED);}
+		if(eventEndDate.getValue().equals("")){eventEndDateLabel.setTextFill(Color.RED); return;}
+		if(descOfEvent.getText().equals("")){eventDescLabel.setTextFill(Color.RED); return;}
+		if(startTime.getValue().equals("")){eventTimeLabel.setTextFill(Color.RED); return;}
+		
+		ArrayList<String> days = new ArrayList<String>();
+		
+		LocalDate start = eventStartDate.getValue();
+		LocalDate end = eventEndDate.getValue();
+		List<LocalDate> totalDates = new ArrayList<>();
+		while (!start.isAfter(end)) {
+		    totalDates.add(start);
+		    start = start.plusDays(1);
+		}
+		
+		for(LocalDate day: totalDates)
+		{
+			days.add(day.format(DateTimeFormatter.ofPattern("yyyy-MM-dd_")) + startTime.getValue());
+		}
+		
+		
+		//TODO pass the days Array List into the below event.		
+		Event newEvent = new Event(eventColor.getValue(), startTime.getValue(), eventPriority.isSelected(), descOfEvent.getText()); 
+		
+		Main.sceneSwitcher("CalendarScreen.fxml");
+	}
+
+}
